c692f561cf4ba5603011c52922620c0391789928
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2020-10-01 22:20:08 +0300

    Delete ft_percent_lower_uppercase(str).py

--- ft_percent_lower_uppercase(str).py
+++
@@ -1,9 +0,0 @@
-def ft_percent_lower_uppercase(str):
-    kolp = int()
-    kols = int()
-    for i in str:
-        if i.isupper():
-            kolp += 1
-        elif i.islower():
-            kols += 1
-    return kolp / kols * 100
