878b5c1990c076a8877742115b4a07a07bb22f08
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2020-10-01 12:26:22 +0300

    Add files via upload

---
+++ ft_even_place(str).py
@@ -0,0 +1,17 @@
+def ft_len(str):
+    kol = int()
+    for i in str:
+        kol += 1
+    return kol
+
+
+
+def ft_even_place(str):
+    str_ = ''
+    res = ft_len(str)
+    for i in range(res):
+        if i % 2 == 0:
+            str_ += str[i]
+    return str_
+
+print(ft_even_place('hello'))
