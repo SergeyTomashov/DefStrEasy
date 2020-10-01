f3c0444287d0c77b0306576d7da8663556289446
SergeyTomashov <72193521+SergeyTomashov@users.noreply.github.com>
2020-10-01 22:20:33 +0300

    Delete ft_first_end_three(str).py

--- ft_first_end_three(str).py
+++
@@ -1,8 +0,0 @@
-def ft_first_end_three(str):
-    kol = int()
-    for i in str:
-        kol += 1
-    if kol > 5:
-        print(str[0], str[1], str[2], str[-3], str[-2], str[-1], sep='')
-    elif kol <= 5:
-        print(str[0] * kol)
